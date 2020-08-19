06b685cef6090e247d208c16c90908b5af236d29
Pablo-Saavedra <elbarto1246@gmail.com>
2020-08-19 12:32:27 -0300

    Frist commit :tada:

---
+++ nightwatch.conf.js
@@ -0,0 +1,28 @@
+module.exports = require('dotenv').config();
+
+module.exports = {
+    'src_folders': ['tests'],
+
+    'webdriver': {
+        'start_process': true,
+        'server_path': "node_modules/chromedriver/lib/chromedriver/chromedriver",
+        'port': 9515
+    },
+
+    'test_settings': {
+        'default': {
+            'screenshots': {
+                'enabled': true,
+                'on_failure': true,
+                'on_error': true,
+                'path': 'tests_output/screenshots'
+            },
+            'desiredCapabilities': {
+                'browserName': 'chrome',
+                'chromeOptions': {
+                    'args': ['--headless']
+                }
+            }
+        }
+    }
+};
